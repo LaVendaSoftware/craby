148471d72e9591d88602c4f0148eccf4b6c6b06a
Walmir <owalmirneto@gmail.com>
2024-06-12 23:02:21 -0300

    chore: change migration_table_name method

--- lib/craby/database_cleaner.rb
+++ lib/craby/database_cleaner.rb
@@ -1,5 +1,19 @@
 require "database_cleaner"
 
+module DatabaseCleaner
+  module ActiveRecord
+    class Base < DatabaseCleaner::Strategy
+      def self.migration_table_name
+        if Gem::Version.new("6.0.0") <= ::ActiveRecord.version
+          ::ActiveRecord::Base.connection_pool.schema_migration.table_name
+        else
+          ::ActiveRecord::SchemaMigration.table_name
+        end
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.before do
     DatabaseCleaner.clean_with(:truncation)
