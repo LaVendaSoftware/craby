f3881e4401fa3dfa7cd5459a75fc9a229128a3bf
Walmir <owalmirneto@gmail.com>
2024-11-06 16:51:28 -0300

    chore: create ENV var to ignore some domain to VCR

--- lib/craby/vcr.rb
+++ lib/craby/vcr.rb
@@ -6,4 +6,10 @@ VCR.configure do |cassette|
   cassette.configure_rspec_metadata!
   cassette.ignore_localhost = true
   # cassette.default_cassette_options = {re_record_interval: 30.days.to_i}
+
+  if ENV["VCR_IGNORED_DOMAINS"].present?
+    cassette.ignore_request do |request|
+      ENV["VCR_IGNORED_DOMAINS"].to_s.split(",").include?(URI(request.uri).host)
+    end
+  end
 end
